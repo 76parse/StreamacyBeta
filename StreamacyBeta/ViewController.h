6feba54d55822858a58d375aa2d05d0f044b4d87
afriedman94 <andrew.friedman94@gmail.com>
2015-05-18 22:06:37 -0400

    Initial Commit

---
+++ StreamacyBeta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StreamacyBeta
+//
+//  Created by Andrew Friedman on 5/18/15.
+//  Copyright (c) 2015 Andrew Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StreamacyBeta/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StreamacyBeta
+//
+//  Created by Andrew Friedman on 5/18/15.
+//  Copyright (c) 2015 Andrew Friedman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
