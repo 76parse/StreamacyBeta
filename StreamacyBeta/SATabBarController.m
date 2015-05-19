69c07be61e2134762d902f502073a1256190b0cd
afriedman94 <andrew.friedman94@gmail.com>
2015-05-19 11:22:10 -0400

    Tab Bar is near completion
    Moving on to Search VC

--- StreamacyBeta/SATabBarController.m
+++ StreamacyBeta/SATabBarController.m
@@ -40,17 +40,12 @@
     item2.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
     playlistsVC.tabBarItem = item2;
     
-    UIViewController *streamVC = viewControllersArray[2];
-    UIImage *tab3Image = [UIImage imageNamed:@"Stream.png"];
-    UITabBarItem *item3 = [[UITabBarItem alloc]initWithTitle:nil image:tab3Image selectedImage:nil];
-    item3.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    streamVC.tabBarItem = item3;
-    
     UIViewController *inboxVC = viewControllersArray[3];
     UIImage *tab4Image = [UIImage imageNamed:@"Inbox.png"];
     UITabBarItem *item4 = [[UITabBarItem alloc]initWithTitle:nil image:tab4Image selectedImage:nil];
     item4.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
     inboxVC.tabBarItem = item4;
+    
     
     UIViewController *FriendsVC = viewControllersArray[4];
     UIImage *tab5Image = [UIImage imageNamed:@"Friends.png"];
@@ -59,17 +54,38 @@
     FriendsVC.tabBarItem = item5;
     
     UIView *tabView = self.view.subviews.lastObject;
-    UIView *purpleLine = [[UIView alloc]initWithFrame:CGRectMake(0, tabView.frame.origin.y-4, self.view.frame.size.width, 4)];
-    [purpleLine setBackgroundColor:[UIColor colorWithRed:0.584 green:0.523 blue:0.979 alpha:0.85]];
+    UIView *purpleLine = [[UIView alloc]initWithFrame:CGRectMake(0, tabView.frame.origin.y-5, self.view.frame.size.width, 5)];
+    [purpleLine setBackgroundColor:[UIColor colorWithRed:0.484 green:0.403 blue:0.884 alpha:1]];
     NSInteger index = self.view.subviews.count-1;
     [self.view insertSubview:purpleLine atIndex:index];
     
     [[UITabBar appearance] setTintColor:[UIColor whiteColor]];
     [[UITabBar appearance] setBarTintColor:[UIColor blackColor]];
+    double buttonWidth = self.view.frame.size.width/5;
+    [[UITabBar appearance] setSelectionIndicatorImage:[AppDelegate imageFromColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:.8] forSize:CGSizeMake(buttonWidth, 49) withCornerRadius:0]];
     
-    [[UITabBar appearance] setSelectionIndicatorImage:[AppDelegate imageFromColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:.8] forSize:CGSizeMake(64, 49) withCornerRadius:0]];
+    UIImage *streamIcon = [UIImage imageNamed:@"StreamIcon.png"];
+    UIImage *streamBackground = [UIImage imageNamed:@"StreamBackground.png"];
+    [self addCenterButtonWithImage:streamIcon highlightImage:streamIcon backgroundImage:streamBackground];
+
+}
 
 
+-(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage backgroundImage:(UIImage *)backgroundImage
+{
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+    double buttonWidth = self.view.frame.size.width/5;
+    button.frame = CGRectMake(0.0, 0.0, buttonWidth, 57);
+    button.adjustsImageWhenHighlighted = NO;
+    
+    [button setImage:buttonImage forState:UIControlStateNormal];
+    [button setImage:highlightImage forState:UIControlStateHighlighted];
+    [button setBackgroundImage:backgroundImage forState:UIControlStateNormal];
+    
+    button.center = self.tabBar.center;
+    
+    [self.view addSubview:button];
 }
 
 
